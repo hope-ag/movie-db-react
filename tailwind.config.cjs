d62fc106c422ad20fbbfb6ba3945c1e8100ba392
Hope <hagyaba@infonaligy.com>
2022-10-01 12:29:25 +0000

    added tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx}", "./index.html"],
+  content: ['./src/**/*.{html,ts,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      primary: colors.indigo,
+      success: colors.emerald,
+      warning: colors.yellow,
+      danger: colors.fuchsia,
+      info: colors.sky,
+    },
     container: {
       center: true,
     },
