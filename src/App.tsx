60f454da6d8367ba9fae5063bbd73a164eeb5bc6
Hope <hagyaba@infonaligy.com>
2022-10-01 11:45:44 +0000

    updated translations

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import i18n from './modules/i18n';
-import { logLanguage } from './services/axiosService';
+// import { searchMovies } from './services/movies';
 
 function App() {
   const { t } = useTranslation();
@@ -14,7 +14,11 @@ function App() {
 
   useEffect(() => {
     i18n.changeLanguage(language)
+    // searchMovies('Batman', 'movie').then(data => {
+    //   console.log(data)
+    // })
   }, [language])
+
   return (
     <div className="container py-6 flex flex-col gap-6">
       Hello
@@ -24,8 +28,6 @@ function App() {
         <option value="en">English</option>
         <option value="fr">French</option>
       </select>
-
-      <button className='w-40 py-3 rounded-full bg-purple-700 text-white' onClick={logLanguage}>Log Language</button>
     </div>
   )
 }
